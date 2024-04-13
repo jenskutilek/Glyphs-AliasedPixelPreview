8167c7418f209214c2fd9950c61a2e226c7c2e49
Jens Kutilek <webmail@kutilek.de>
2024-04-13 19:44:47 +0200

    Make it work

--- AliasedPixelPreview.glyphsReporter/Contents/Resources/plugin.py
+++ AliasedPixelPreview.glyphsReporter/Contents/Resources/plugin.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
 import objc
-from AppKit import NSBezierPath, NSColor
+from AppKit import NSBezierPath, NSClassFromString, NSColor, NSRect
 from GlyphsApp import Glyphs
 from GlyphsApp.plugins import ReporterPlugin
+
+
+PIXEL_COLOR = (0, 0, 0, 0.15)
 
 
 class AliasedPixelPreview(ReporterPlugin):
@@ -18,19 +21,30 @@ class AliasedPixelPreview(ReporterPlugin):
 
     @objc.python_method
     def background(self, layer):
-        NSColor.controlTextColor().set()
+        currentController = self.controller.view().window().windowController()
+        if currentController:
+            tool = currentController.toolDrawDelegate()
+            # don't activate if on cursor tool, or pan tool
+            if (
+                tool.isKindOfClass_(NSClassFromString("GlyphsToolText"))
+                or tool.isKindOfClass_(NSClassFromString("GlyphsToolHand"))
+                or tool.isKindOfClass_(
+                    NSClassFromString("GlyphsToolTrueTypeInstructor")
+                )
+            ):
+                return
+
+        NSColor.colorWithCalibratedRed_green_blue_alpha_(*PIXEL_COLOR).set()
         outline = layer.completeBezierPath
-        # bounds = layer.bounds
-        # <CoreFoundation.CGRect
-        #     origin=<CoreFoundation.CGPoint x=100.0 y=-0.04>
-        #     size=<CoreFoundation.CGSize width=1200.0 height=1000>>
         font = layer.font()
         step = font.upm // 18
         halfstep = step // 2
         for y in range(1200 // step, -400 // step, -1):
             for x in range(0, int(layer.width // step)):
                 if outline.containsPoint_((x * step + halfstep, y * step + halfstep)):
-                    NSBezierPath.fillRect_((x * step, y * step), (step, step))
+                    NSBezierPath.fillRect_(
+                        NSRect(origin=(x * step, y * step), size=(step, step))
+                    )
 
     @objc.python_method
     def __file__(self):
